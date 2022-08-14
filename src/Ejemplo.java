f33213efccc3b3c106b1b5b8b0c1cbbd9d54b646
Julio Alberto de la Teja López <jdelatejal242@profesor.uaemex.mx>
2022-08-13 21:00:48 -0500

    segundo commit

--- src/Ejemplo.java
+++ src/Ejemplo.java
@@ -1,5 +1,9 @@
 public class Ejemplo {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        int a=10;
+        a=a+1;
+        System.out.println("el valor es ");
+        System.out.println(a);
     }
 }
